794fb1e6fdb3cf008b7d108ca005dea877e8aa72
so-SKD <sarah@stellakinesisdesign.com>
2026-03-04 18:22:53 -0500

    save APIs section

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,10 +15,20 @@ urlpatterns = [
     path('logout', views.logout_request, name='logout'),  
 
     # path for dealer reviews view
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
 
     # path for add a review view
 
     # path to get cars
     path('get_cars/', views.get_cars, name='getcars'),
+
+    # path to get dealers
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
+    # path to get dealer details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+
+    path(route='add_review', view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
