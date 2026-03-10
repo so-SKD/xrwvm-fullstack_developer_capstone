569c8802924500321c053a10d54c1afe5afe29f0
so-SKD <sarah@stellakinesisdesign.com>
2026-03-10 18:04:22 -0400

    lint js save 1

--- server/database/app.js
+++ server/database/app.js
@@ -1,8 +1,9 @@
+/* eslint-env es6 */
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
 app.use(cors())
@@ -13,19 +14,17 @@ const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'))
 
 mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
 
-
 const Reviews = require('./review');
-
 const Dealerships = require('./dealership');
 
 try {
-  Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
+  Reviews.deleteMany({}).then(() => {
+    Reviews.insertMany(reviews_data.reviews); 
   });
-  Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
+  Dealerships.deleteMany({}).then(() => {
+    Dealerships.insertMany(dealerships_data.dealerships); 
   });
-  
+
 } catch (error) {
   res.status(500).json({ error: 'Error fetching documents' });
 }
